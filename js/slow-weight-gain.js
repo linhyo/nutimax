0f459936f7767fca35267fad1dc2373e371e4183
Nguyen Dieu Linh <linh.nguyendieu@jmango360.com>
2016-04-22 03:15:44 +0700

    Slow weight gain page - mobile version

--- js/slow-weight-gain.js
+++ js/slow-weight-gain.js
@@ -1,5 +1,28 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
+  $('#light-slider').lightSlider({
+    item: 4,
+    loop: true,
+    slideMove: 2,
+    slideMargin: 20,
+    easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+    speed: 600,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          item: 3,
+          slideMove: 1,
+          slideMargin: 10
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          item: 1,
+          slideMove: 1,
+          slideMargin: 0
+        }
+      }
+    ]
+  });
 });
-
-
