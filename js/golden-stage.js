3d1bbbe4aa7ed0a47257085fc5d7a75d66f97178
Nguyen Dieu Linh <linh.nguyendieu@jmango360.com>
2016-04-23 08:21:31 +0700

    Landing page

--- js/golden-stage.js
+++ js/golden-stage.js
@@ -68,4 +68,16 @@ $(function () {
     }).attr('src', source);
   });
 
+
+  var wow = new WOW(
+    {
+      boxClass: 'wow',      // default
+      animateClass: 'animated', // default
+      offset: 0,          // default
+      mobile: true,       // default
+      live: true        // default
+    }
+  );
+  wow.init();
+
 });
